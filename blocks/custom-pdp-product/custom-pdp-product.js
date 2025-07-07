37152be732b5cc1a1e549cb484ba54577ea776eb
Prathamesh Giri <gauravgiri626@gmail.com>
2025-07-07 06:45:14 +0530

    Added logs for validation

--- blocks/custom-pdp-product/custom-pdp-product.js
+++ blocks/custom-pdp-product/custom-pdp-product.js
@@ -1,11 +1,12 @@
 
 function createProduct($block, props = {}){
+    console.log("Create Product called")
     const product = props?.product || {};
     const title = product.title || 'No Title';
     const sku = product.sku || 'No SKU';
     const price = product.price || '0';
     // const description = product.description || ' ';
-
+    console.log(title+" "+sku+" "+price)
     const wrapper = document.createElement('div')
     wrapper.className = 'custom-pdp-product-banner'
     wrapper.innerHTML = `
